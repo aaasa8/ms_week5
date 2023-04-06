7c251f27d1085a50dc3965c3caf5f848c81cd6d5
aaasa8 <113534445+aaasa8@users.noreply.github.com>
2023-04-06 17:41:00 +0900

    Update MyCustomView.kt

--- app/src/main/java/com/example/week5/MyCustomView.kt
+++ app/src/main/java/com/example/week5/MyCustomView.kt
@@ -1,10 +1,18 @@
 package com.example.week5
 
 import android.view.View
+import java.util.Random
 
 class MyCustomView : View {
 
 	var color : Integer = Color.RED	
+	
+	val random = Random()
+
+	val x = random.nextInt(500)
+	val y = random.nextInt(500)
+
+	val r = (random.nextInt(2) + 1) * 100
 
 	constructor(context: Context?) : super(context)
 	constructor(context: Context?, attrs: AttributeSet?): super(context, attrs)
@@ -16,7 +24,7 @@ class MyCustomView : View {
 			canvas?.drawColor(Color.LTGRAY)
 			val paint = Paint()
 			paint.setColor(color)
-			canvas?.drawCircle(200.toFloat(), 200.toFloat(), 100.toFloat(), paint)	
+			canvas?.drawCircle(x.toFloat(), y.toFloat(), r.toFloat(), paint)	
 	}
 	
 }
