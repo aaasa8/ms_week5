106bd4a9f95a543213ea2fb6d1def0820018c26c
aaasa8 <113534445+aaasa8@users.noreply.github.com>
2023-04-06 17:37:43 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/week5/MainActivity.kt
+++ app/src/main/java/com/example/week5/MainActivity.kt
@@ -11,9 +11,13 @@ class MainActivity : AppCompatActivity() {
 				//setContentView(myView)
 
 				val button = findViewById<Button>(R.id.button)
-
+				val myCustomView = findViewById<MyCustomView>(R.id.myCustomView)
+			
+				//onDraw는 뷰가 그려지는 상황에 호출. but 마음대로 호출할 수 X.
+				//뷰가 다시 그려지는 상황 만들기 -> Invalidate() 이용.
 				button.setOnClickListener {
-					
+					myCustomView.color = Color.YELLOW
+					myCustomView.invaliate()
 				}
     }
 
