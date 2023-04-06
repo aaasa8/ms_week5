122cf6c14c49d04006e3ba77e836d8364a30d915
aaasa8 <113534445+aaasa8@users.noreply.github.com>
2023-04-06 17:02:56 +0900

    Update MyCustomView.kt

--- app/src/main/java/com/example/week5/MyCustomView.kt
+++ app/src/main/java/com/example/week5/MyCustomView.kt
@@ -4,12 +4,16 @@ import android.view.View
 
 class MyCustomView : View {
 	constructor(context: Context?) : super(context)
-	constructor(context: Context?, attrSet: AttributeSet?): super(context, attrSet)
-	constructor(context: Context?, attrSet: AttributeSet?, defStyleSet: Int)
-		: super(context, attrSet, defStyleAttrSet)
+	constructor(context: Context?, attrs: AttributeSet?): super(context, attrs)
+	constructor(context: Context?, attrs: AttributeSet?, defStyleSet: Int)
+		: super(context, attrs, defStyleAttrSet)
 
 	override fun onDraw(canvas: Canvas?) {
-		
+			super.onDraw(canvas)
+			canvas?.drawColor(Color.LTGRAY)
+			val paint = Paint()
+			paint.setColor(Color.RED)
+			canvas?.drawCircle(200.toFloat(), 200.toFloat(), 100.toFloat(), paint)	
 	}
 	
 }
