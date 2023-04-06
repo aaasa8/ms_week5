7c8dbd0e04d67827ff03b5873cadfaab68322ddb
aaasa8 <113534445+aaasa8@users.noreply.github.com>
2023-04-06 16:45:33 +0900

    Update MyCustomView.kt

--- app/src/main/java/com/example/week5/MyCustomView.kt
+++ app/src/main/java/com/example/week5/MyCustomView.kt
@@ -1,4 +1,13 @@
 package com.example.week5
 
-class MyCustomView {
+class MyCustomView : View {
+	constructor(context: Context?) : super(context)
+	constructor(context: Context?, attrSet: AttributeSet?): super(context, attrSet)
+	constructor(context: Context?, attrSet: AttributeSet?, defStyleSet: Int)
+		: super(context, attrSet, defStyleAttrSet)
+
+	override fun onDraw(canvas: Canvas?) {
+		
+	}
+	
 }
