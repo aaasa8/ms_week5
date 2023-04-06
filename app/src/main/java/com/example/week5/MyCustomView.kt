40064f1140cb2a2e037336b18514b505b997c4e4
aaasa8 <113534445+aaasa8@users.noreply.github.com>
2023-04-06 17:09:59 +0900

    Update MyCustomView.kt

--- app/src/main/java/com/example/week5/MyCustomView.kt
+++ app/src/main/java/com/example/week5/MyCustomView.kt
@@ -3,6 +3,9 @@ package com.example.week5
 import android.view.View
 
 class MyCustomView : View {
+
+	var color : Integer = Color.RED	
+
 	constructor(context: Context?) : super(context)
 	constructor(context: Context?, attrs: AttributeSet?): super(context, attrs)
 	constructor(context: Context?, attrs: AttributeSet?, defStyleSet: Int)
@@ -12,7 +15,7 @@ class MyCustomView : View {
 			super.onDraw(canvas)
 			canvas?.drawColor(Color.LTGRAY)
 			val paint = Paint()
-			paint.setColor(Color.RED)
+			paint.setColor(color)
 			canvas?.drawCircle(200.toFloat(), 200.toFloat(), 100.toFloat(), paint)	
 	}
 	
