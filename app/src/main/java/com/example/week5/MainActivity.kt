1df3dc551c9c23a38778cc17e953d65e81617aa1
DDWU <nn00rnrn@naver.com>
2023-04-09 15:36:37 +0900

    c

--- app/src/main/java/com/example/week5/MainActivity.kt
+++ app/src/main/java/com/example/week5/MainActivity.kt
@@ -1,40 +1,52 @@
 package com.example.week5
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-				//val myView = MyView(this)
-				//setContentView(myView)
+        setContentView(R.layout.activity_main)
+		//		val myView = MyCustomView(this)
+		//		setContentView(myView)
 
-				val random = Random()
-				val x = random.nextInt(500)
-				val y = random.nextInt(500)
-				val r = (random.nextInt(2) + 1) * 100
 
-				val button = findViewById<Button>(R.id.button)
-				val myCustomView = findViewById<MyCustomView>(R.id.myCustomView)
+
+		val button = findViewById<Button>(R.id.button)
+		val myCustomView = findViewById<MyCustomView>(R.id.myCustomView)
+
 			
-				//onDraw는 뷰가 그려지는 상황에 호출. but 마음대로 호출할 수 X.
-				//뷰가 다시 그려지는 상황 만들기 -> Invalidate() 이용.
-				button.setOnClickListener {
-					myCustomView.drawCircle(x.toFloat(), y.toFloat(), r.toFloat(), paint)
-					Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
-					myCustomView.invaliate()
-				}
+	//onDraw는 뷰가 그려지는 상황에 호출. but 마음대로 호출할 수 X.
+	//뷰가 다시 그려지는 상황 만들기 -> Invalidate() 이용.
+		button.setOnClickListener {
+			val random = Random()
+			val x = random.nextInt(500)
+			val y = random.nextInt(500)
+			val r = (random.nextInt(2) + 1) * 100
+
+			myCustomView.x = x
+			myCustomView.y = y
+			myCustomView.r = r
+
+			myCustomView.invalidate()
+		}
     }
 
-		/*class MyView : View(context) { //ctrl + 5
+	/*	inner class MyView : View(context) { //ctrl + 5
 			override fun onDraw(canvas: Canvas?) {
 				super.onDraw(canvas)
-				canvas?.drawColor(Color.LTGRAY)
+				canvas?.drawColor(Color.YELLOW)
 				val paint = Paint()
-				paint.setColor(Color.BLUE)
-				canvas?.drawCircle(200.toFloat(), 200.toFloat(), 100.toFloat(), paint)
+				paint.setColor(Color.CYAN)
+				canvas?.drawCircle(x.toFloat(), y.toFloat(), r.toFloat(), paint)
+				Toast.makeText(this@MainActivity, x + y + r, Toast.LENGTH_SHORT).show()
 			}
 		}*/
 }
--- app/src/main/java/com/example/week5/MyCustomView.kt
+++ app/src/main/java/com/example/week5/MyCustomView.kt
@@ -1,22 +1,32 @@
 package com.example.week5
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
 import android.view.View
+import android.widget.Toast
+import java.util.*
 
 class MyCustomView : View {
 
-	var color : Integer = Color.CYAN	
-	
 	constructor(context: Context?) : super(context)
 	constructor(context: Context?, attrs: AttributeSet?): super(context, attrs)
 	constructor(context: Context?, attrs: AttributeSet?, defStyleSet: Int)
-		: super(context, attrs, defStyleAttrSet)
+		: super(context, attrs, defStyleSet)
+
+	var x : Int = 100
+	var y : Int = 100
+	var r : Int = 100
 
 	override fun onDraw(canvas: Canvas?) {
-			super.onDraw(canvas)
-			canvas?.drawColor(Color.YELLOW)
-			val paint = Paint()
-			//paint.setColor(color)
-			canvas?.drawCircle(100.toFloat(), 100.toFloat(), r.toFloat(), paint)	
+		super.onDraw(canvas)
+		canvas?.drawColor(Color.YELLOW)
+		val paint = Paint()
+		paint.setColor(Color.CYAN)
+		canvas?.drawCircle(x.toFloat(), y.toFloat(), r.toFloat(), paint)
+
 	}
-	
+
 }
