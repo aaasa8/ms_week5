95bd39b2920b221601c5d830fecc783645578661
aaasa8 <113534445+aaasa8@users.noreply.github.com>
2023-04-09 13:26:50 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/week5/MainActivity.kt
+++ app/src/main/java/com/example/week5/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.week5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,13 +11,19 @@ class MainActivity : AppCompatActivity() {
 				//val myView = MyView(this)
 				//setContentView(myView)
 
+				val random = Random()
+				val x = random.nextInt(500)
+				val y = random.nextInt(500)
+				val r = (random.nextInt(2) + 1) * 100
+
 				val button = findViewById<Button>(R.id.button)
 				val myCustomView = findViewById<MyCustomView>(R.id.myCustomView)
 			
 				//onDraw는 뷰가 그려지는 상황에 호출. but 마음대로 호출할 수 X.
 				//뷰가 다시 그려지는 상황 만들기 -> Invalidate() 이용.
 				button.setOnClickListener {
-					myCustomView.color = Color.YELLOW
+					myCustomView.drawCircle(x.toFloat(), y.toFloat(), r.toFloat(), paint)
+					Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
 					myCustomView.invaliate()
 				}
     }
